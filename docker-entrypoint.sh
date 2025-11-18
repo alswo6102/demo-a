3c0417f174e7f2f57a63885d9bc5d194da07e8bc
LEE MINJAE <alswo6102@gmail.com>
2025-11-18 11:05:59 +0900

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# envsubst로 템플릿을 실제 설정으로 렌더링
-envsubst '\$PORT \$API_TARGET' \
-  < /etc/nginx/templates/default.conf.template \
-  > /etc/nginx/conf.d/default.conf
-
-exec nginx -g 'daemon off;'
